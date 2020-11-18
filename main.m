c2b92c9f8efbac3ae64bdf3b11c014dd60e658ef
Yuxuan <yuxuan.wu17@gmail.com>
2020-11-18 18:39:26 +0800

    finished the for loop

--- main.m
+++ main.m
@@ -5,13 +5,14 @@ y = ind2vec(phoglabels')';
 X = phogsamples;
 
 %% split data into 8:2
-[ndata, D] = size(X);        %ndata样本数，D维数
-R = randperm(ndata);         %1到n这些数随机打乱得到的一个随机数字序列作为索引
+rng(1)
+[ndata, D] = size(X);        %ndata，D: feature dimension
+R = randperm(ndata);         %1到n randomize the number as index
 num_test = floor(ndata*0.2);
-X_test = X(R(1:num_test),:);  %以索引的前1000个数据点作为测试样本Xtest
+X_test = X(R(1:num_test),:);  
 R(1:num_test) = [];
-X_train = X(R,:);          %剩下的数据作为训练样本Xtraining
-num_training = size(X_train,1);%num_training；训练样本数
+X_train = X(R,:);          
+num_training = size(X_train,1);
 
 y_test = y(R(1:num_test),:);
 y_train = y(R,:);
@@ -31,18 +32,69 @@ target_ts = y_test;
 
 %% train NN
 
+% seperate the value 
+neurons = [32,64,128];
+% lr_rate = 0.1;
+% mc_rate = 0.6;
+
+lr_rate = [0.1,0.4,0.7];
+mc_rate = [0.6,0.75,0.9];
+
+for i = neurons 
+    for j = lr_rate
+        for k = mc_rate
+            % build the NN
+            nhidden = i; %number of hidden layers
+            net=newff(data_tr,target_tr,[nhidden,nhidden,nhidden],{'logsig','logsig','logsig','logsig'},'traingd');
+            % set the hyperparameter (epochs, learning rate)
+            net.trainParam.epochs = 500; %number of training epochs
+            net.trainParam.lr = j;
+            net.trainParam.mc = k;
+            % train a neural network
+            net = train(net,data_tr,target_tr);
+            % predict the value
+            y_predict = sim(net,data_ts);
+            Y_predict = mapminmax('reverse',y_predict,y_output);
+            perf = perform(net, y_test, Y_predict);
+            fprintf('value of mse in neuron %d, learning rate %2.1f, momentum is %3.1f,is %6.4f\n ',i,j,k,perf);
+        end 
+    end 
+end 
+
+
+
+
+% show network
+
+% view(net);
+
+
+% 
+% for i = neurons
+%     for j = lr_rate
+%         for k = mc_rate
+%             fprintf('value of i:%d\n,value of j:%2.1f\n,value of k:%3.1f\n',i,j,k)
+%         end
+% %         fprintf('value of j:%d\n',j)
+%     end
+% %     fprintf('value of i: %d\n', i);
+% end
+%%
+
 % build the NN
-nhidden = 25; %number of hidden layers
-net=newff(data_tr,target_tr,[nhidden,nhidden],{'logsig','logsig','logsig'},'traingd');
+nhidden = 32; %number of hidden layers
+net=newff(data_tr,target_tr,[nhidden,nhidden,nhidden],{'logsig','logsig','logsig','logsig'},'traingd');
 % set the hyperparameter (epochs, learning rate)
-net.trainParam.epochs = 1000; %number of training epochs
+net.trainParam.epochs = 500; %number of training epochs
 net.trainParam.lr = 0.1;
-net.trainParam.mc = 0.9;
+net.trainParam.mc = 0.7;
 
 % train a neural network
 [net,tr] = train(net,data_tr,target_tr);
 % show network
-view(net);
+
+% view(net);
+
 % predict the value
 y_predict = sim(net,data_ts);
 Y_predict = mapminmax('reverse',y_predict,y_output);
@@ -54,14 +106,14 @@ perf = perform(net, y_test, Y_predict)
 
 %% watch the distribution of predict density and y_test density
 num1 = vec2ind(Y_predict); % Predicted groups
-num2 = vec2ind(y_test) % Known groups
-figure(1)
-[f,xi]=ksdensity(num1);
-plot(xi,f)
-
-figure(2)
-[f,xi]=ksdensity(num2);
-plot(xi,f);
+num2 = vec2ind(y_test); % Known groups
+% figure(1)
+% [f,xi]=ksdensity(num1);
+% plot(xi,f)
+% 
+% figure(2)
+% [f,xi]=ksdensity(num2);
+% plot(xi,f);
 
 %% Create the confusion matrix
 % size(num1,2)
@@ -73,7 +125,7 @@ confusionchart(C)
 
 %% evaluation of the model 
 figure(3)
-N = size(y_test,2)
+N = size(y_test,2);
 plot(1:N,vec2ind(Y_predict),':b*',1:N,vec2ind(y_test))
 legend('预测输出','期望输出')
 title('BP网络预测输出','fontsize',12)
